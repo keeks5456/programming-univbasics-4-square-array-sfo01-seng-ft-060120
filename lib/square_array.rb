de34ac18ffaf6bf1ac923435aee221cba5d96a68
Akiko Green <akikogreen08@gmail.com>
2020-04-26 01:34:53 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,5 @@
 require 'pry'
 
 def square_array(array)
-  binding.pry
-count = 0 
-newArray = []
-while count < array.length do
-  newArray.push(array[count] + 1)
-end 
-  return newArray
+  
 end
